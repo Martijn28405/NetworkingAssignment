08db9ecdfea17ab4ce927a6d0c3f427fca59991a
Martijn28405 <1049334@hr.nl>
2024-03-30 12:40:31 +0100

    Template Code Networking Assignment

---
+++ AssignmentFirstAttempt(Student)/client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using MessageNS;
+
+// SendTo();
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClientUDP cUDP = new ClientUDP();
+        cUDP.start();
+    }
+}
+
+class ClientUDP
+{
+
+    //TODO: implement all necessary logic to create sockets and handle incoming messages
+    // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+    public void start()
+    {
+
+    }
+    //TODO: create all needed objects for your sockets 
+
+    //TODO: [Send Hello message]
+
+    //TODO: [Receive Welcome]
+
+    //TODO: [Send RequestData]
+
+    //TODO: [Receive Data]
+
+    //TODO: [Send RequestData]
+
+    //TODO: [Send End]
+
+    //TODO: [Handle Errors]
+
+
+}
