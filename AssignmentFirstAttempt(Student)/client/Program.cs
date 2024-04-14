472fff243980ba954d291e542cd5dcaa7f0c0268
Martijn28405 <1049334@hr.nl>
2024-04-14 15:47:23 +0200

    Tried to make a setup  for the assignment needs

--- AssignmentFirstAttempt(Student)/client/Program.cs
+++ AssignmentFirstAttempt(Student)/client/Program.cs
@@ -1,44 +1,129 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using MessageNS;
 
-// SendTo();
-class Program
-{
-    static void Main(string[] args)
-    {
-        ClientUDP cUDP = new ClientUDP();
-        cUDP.start();
-    }
-}
 
+class Program{
+    static void Main(string[] args){
+        ClientUDP cUDP = new ClientUDP();
+        cUDP.Start();
+    }
+
+}
 class ClientUDP
 {
+    private UdpClient udpClient;
+    private IPEndPoint serverEndPoint;
 
-    //TODO: implement all necessary logic to create sockets and handle incoming messages
-    // Do not put all the logic into one method. Create multiple methods to handle different tasks.
-    public void start()
+    public void Start()
     {
+        udpClient = new UdpClient();
+        serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 32000);
+
+        SendHello();
+    }
+
+    private void SendHello()
+    {
+        try
+        {
+            Message helloMessage = new Message
+            {
+                Type = MessageType.Hello,
+                Content = "Hello"
+            };
+            byte[] helloBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(helloMessage));
+            udpClient.Send(helloBytes, helloBytes.Length, serverEndPoint);
+            Console.WriteLine($"Sent hello message");
+            ReceiveWelcome();
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error sending hello message: {ex.Message}");
+        }
+    }
+
+    private void ReceiveWelcome()
+    {
+        try
+        {
+            byte[] welcomeBytes = udpClient.Receive(ref serverEndPoint);
+            string welcomeMessage = Encoding.ASCII.GetString(welcomeBytes);
+            Message welcome = JsonSerializer.Deserialize<Message>(welcomeMessage);
+            if (welcome.Type == MessageType.Welcome)
+            {
+            Console.WriteLine($"Received welcome message: {welcome.Content}");
+            SendRequestData();
+            }
+            else
+            {
+            Console.WriteLine($"Received unexpected message type: {welcome.Type}");
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error receiving welcome message: {ex.Message}");
+        }
 
     }
-    //TODO: create all needed objects for your sockets 
 
-    //TODO: [Send Hello message]
+    private void SendRequestData()
+    {
+        try
+        {
+            Message requestDataMessage = new Message
+            {
+                Type = MessageType.RequestData,
+                Content = "Request data"
+            };
+            byte[] requestDataBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(requestDataMessage));
+            udpClient.Send(requestDataBytes, requestDataBytes.Length, serverEndPoint);
+            Console.WriteLine($"Sent request data message");
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error sending request data message: {ex.Message}");
+        }
+    }
 
-    //TODO: [Receive Welcome]
+    private void ReceiveData()
+    {
+        try
+        {
+            byte[] dataBytes = udpClient.Receive(ref serverEndPoint);
+            string dataMessage = Encoding.ASCII.GetString(dataBytes);
+            Message data = JsonSerializer.Deserialize<Message>(dataMessage);
+            Console.WriteLine($"Received data: {data.Content}");
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error receiving data: {ex.Message}");
+        }
+    }
 
-    //TODO: [Send RequestData]
-
-    //TODO: [Receive Data]
-
-    //TODO: [Send RequestData]
-
-    //TODO: [Send End]
-
-    //TODO: [Handle Errors]
-
-
+    private void SendEnd()
+    {
+        try
+        {
+            Message endMessage = new Message
+            {
+                Type = MessageType.End,
+                Content = "End"
+            };
+            byte[] endBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(endMessage));
+            udpClient.Send(endBytes, endBytes.Length, serverEndPoint);
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error sending end message: {ex.Message}");
+        }
+    }
 }
+
+    //TODO: Implement error handling logic
+
+    //TODO: Implement error handling logic
--- AssignmentFirstAttempt(Student)/server/Program.cs
+++ AssignmentFirstAttempt(Student)/server/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Data.SqlTypes;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Text.Json;
 using MessageNS;
@@ -20,34 +18,141 @@ class Program
 
 class ServerUDP
 {
-
     //TODO: implement all necessary logic to create sockets and handle incoming messages
-    // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+     //TODO: create all needed objects for your sockets
+        static UdpClient udpClient = new UdpClient(32000);
+        static byte[] buffer = new byte[1024];
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        static IPEndPoint endPoint = new IPEndPoint(ipAddress, 32000);
+       
     public void start()
     {
-
+        Console.WriteLine("Server is listening on port 32000");
+        HandleMessage(Encoding.ASCII.GetString(udpClient.Receive(ref endPoint)));
     }
 
-    //TODO: create all needed objects for your sockets 
 
-    //TODO: keep receiving messages from clients
-    // you can call a dedicated method to handle each received type of messages
+        //TODO: keep receiving messages from clients
+        // you can call a dedicated method to handle each received type of messages
 
-    //TODO: [Receive Hello]
-
-    //TODO: [Send Welcome]
-
-    //TODO: [Receive RequestData]
-
-    //TODO: [Send Data]
-
-    //TODO: [Implement your slow-start algorithm considering the threshold] 
-
-    //TODO: [End sending data to client]
-
-    //TODO: [Handle Errors]
-
-    //TODO: create all needed methods to handle incoming messages
+        
 
 
+        //TODO: [Send Data]
+
+        //TODO: [Implement your slow-start algorithm considering the threshold] 
+
+        //TODO: [End sending data to client]
+
+        //TODO: [Handle Errors]
+
+        //TODO: create all needed methods to handle incoming messages
+
+    private static void SendWelcome()
+    {
+        // Create a Message object and set its properties
+        Message welcomeMessage = new Message
+        {
+            Type = MessageType.Welcome,
+            Content = "Welcome to the server!"
+        };
+    
+        // Convert the Message object to a JSON string
+        string json = JsonSerializer.Serialize(welcomeMessage);
+    
+        // Convert the JSON string to a byte array
+        byte[] buffer = Encoding.ASCII.GetBytes(json);
+    
+        // Send the byte array to the client
+        socket.SendTo(buffer, endPoint);
+    }
+
+    private void HandleMessage(string message)
+    {
+
+        while(true){
+            // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+            // TODO: Implement logic to handle the received message
+            switch ((MessageType)JsonSerializer.Deserialize<Message>(message).Type)
+            {
+                case MessageType.Hello:
+              {
+            // Handle the hello message
+            // make sure the program continues and does not come in a loop 
+            Console.WriteLine("Received hello message from client");
+
+            // Send a welcome message to the client
+            SendWelcome();
+            HandleMessage(Encoding.ASCII.GetString(udpClient.Receive(ref endPoint)));
+            break;
+        }
+
+                case MessageType.RequestData:
+                    Console.WriteLine("Received request data from client");
+                    SendData();
+                    break;
+                case MessageType.Data:
+                Console.WriteLine("Received data from client");
+                    break;
+                case MessageType.End:
+                    EndSendingData();
+                    break;
+                default:
+                    HandleErrors();
+                    break;
+            }
+        }
+    }
+
+    private void ReceiveRequestData(Socket socket)
+    {
+        //TODO: [Receive RequestData]
+        // TODO: Implement logic to receive request data from the client
+        byte[] buffer = new byte[1024];
+        // TODO: Use the appropriate socket to receive the request data
+        int bytesRead = socket.Receive(buffer);
+        string requestData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        // TODO: Process the received request data
+        Console.WriteLine("Received request data from client: " + requestData);
+    }
+
+    private void SendData()
+    {
+        // TODO: Implement logic to send data to the client
+        string data = "This is the data to send";
+        byte[] buffer = Encoding.ASCII.GetBytes(data);
+        // TODO: Use the appropriate socket to send the data to the client
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 32000);
+        socket.SendTo(buffer, endPoint);
+    }
+
+    private void ImplementSlowStartAlgorithm()
+    {
+        // TODO: Implement logic for the slow-start algorithm considering the threshold
+        int threshold = 10;
+        int currentWindowSize = 1;
+        // TODO: Implement the slow-start algorithm using the current window size and threshold
+        while (currentWindowSize < threshold)
+        {
+            // Send data to the client
+            SendData();
+            currentWindowSize *= 2;
+        }
+    }
+
+    private void EndSendingData()
+    {
+        // TODO: Implement logic to end sending data to the client
+        // TODO: Use the appropriate socket to close the connection
+        socket.Close();
+    }
+
+    private void HandleErrors()
+    {
+        // TODO: Implement logic to handle errors
+        // TODO: Handle any exceptions or error conditions that may occur
+    }
 }
