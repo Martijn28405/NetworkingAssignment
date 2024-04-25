389c2d29ebfca9538968e33b876b6f5270fbbed5
DoubleKimp <karstenkeemink@gmail.com>
2024-04-25 20:40:36 +0200

    bruh mannn

--- AssignmentFirstAttempt(Student)/client/Program.cs
+++ AssignmentFirstAttempt(Student)/client/Program.cs
@@ -17,26 +17,134 @@ class Program
 
 class ClientUDP
 {
+    //TODO: create all needed objects for your sockets
+    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    byte[] buffer = new byte[66000];
+    static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    static IPEndPoint ServerEndpoint = new IPEndPoint(ipAddress, 32000);
+    static EndPoint remoteEP = new IPEndPoint(ipAddress, 32000);
 
+    public int threshold = 20;
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
-    {
 
+    {
+        SendHelloMessage();
+        ReceiveMessage();
     }
-    //TODO: create all needed objects for your sockets 
 
     //TODO: [Send Hello message]
+    public void SendHelloMessage()
+    {
+        s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        Message msg = new Message();
+        msg.Type = MessageType.Hello;
+        msg.Content = threshold.ToString(); // Default threshold value
+        string json = JsonSerializer.Serialize(msg);
+        byte[] data = Encoding.ASCII.GetBytes(json);
+        s.SendTo(data, ServerEndpoint);
+        Console.WriteLine("Hello message sent to the server");
+    }
+
+    public void ReceiveMessage()
+    {
+        while (true)
+        {
+            int recv = s.ReceiveFrom(buffer, ref remoteEP);
+            string message = Encoding.ASCII.GetString(buffer, 0, recv);
+            Message msg = JsonSerializer.Deserialize<Message>(message);
+            switch (msg.Type)
+            {
+                case MessageType.Welcome:
+                    Console.WriteLine("Welcome message received");
+                    SendRequestData();
+                    break;
+                case MessageType.Data:
+                    HandleData(msg);
+
+                    break;
+                case MessageType.End:
+                    HandleEnd();
+                    break;
+                    // case MessageType.Error:
+                    //     HandleError();
+                    //     break;
+            }
+
+        }
+    }
 
     //TODO: [Receive Welcome]
 
     //TODO: [Send RequestData]
+    public void SendRequestData()
+    {
+        Message msg = new Message();
+        msg.Type = MessageType.RequestData;
+        msg.Content = "hamlet.txt";
+        string json = JsonSerializer.Serialize(msg);
+        byte[] data = Encoding.ASCII.GetBytes(json);
+        s.SendTo(data, ServerEndpoint);
+        Console.WriteLine("RequestData message sent to the server");
+    }
 
     //TODO: [Receive Data]
+    public void HandleData(Message msg)
+    {
+        Console.WriteLine("Data message received");
+        try
+        {
+            string msgID = msg.Content.Substring(0, 4);
+            string msgData = msg.Content.Substring(4);
+            if (msgID == "0000")
+            {
+                File.Delete("output.txt");
+            }
+            File.AppendAllText("output.txt", msgData);
+            SendAck(msgID);
+        }
+        catch
+        {
+            Console.WriteLine("Error: message is empty");
+            //handleerror
+        }
 
-    //TODO: [Send RequestData]
+
+    }
+
+    public void SendAck(string id)
+    {
+        Message msg = new Message
+        {
+            Type = MessageType.Ack,
+            Content = id
+        };
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
+        s.SendTo(msgBytes, ServerEndpoint);
+        Console.WriteLine("Ack message sent to the server");
+    }
 
     //TODO: [Send End]
+    public void HandleEnd()
+    {
+        Console.WriteLine("End message received");
+        // Perform any necessary cleanup or finalization here
+        // For example, close the socket or release any resources
+        Environment.Exit(0);
+
+    }
+
+    //TODO: [Send Error]
+    public void HandleError()
+    {
+        Console.WriteLine("Error message received");
+
+        // Perform any necessary error handling here
+
+
+    }
+
 
     //TODO: [Handle Errors]
 
--- AssignmentFirstAttempt(Student)/server/Program.cs
+++ AssignmentFirstAttempt(Student)/server/Program.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using MessageNS;
+using Microsoft.VisualBasic;
 
 
 // Do not modify this class
@@ -20,30 +21,186 @@ class Program
 
 class ServerUDP
 {
+    //TODO: create all needed objects for your sockets 
+    private byte[] buffer = new byte[66000];
+    private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    private static IPEndPoint serverIpEndPoint = new IPEndPoint(ipAddress, 32000);
+    private EndPoint remoteEP = new IPEndPoint(ipAddress, 32000);
+
+    Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+    private static int threshold = 0;
+    private int congestionWindow = threshold;
+    private int duplicateAckCount = 0;
+
 
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
     {
+        sock.Bind(serverIpEndPoint);
+        ReceiveMessage();
+    }
+
+    //TODO: keep receiving messages from clients
+    public void ReceiveMessage()
+    {
+        Console.WriteLine("server is listening on port 32000");
+
+        while (true)
+        {
+            Console.WriteLine("server is waiting for a message.....");
+            int recv = sock.ReceiveFrom(buffer, ref remoteEP);
+            string message = Encoding.ASCII.GetString(buffer, 0, recv);
+            Console.WriteLine("Received message: " + message + "\n");
+            Message msg = JsonSerializer.Deserialize<Message>(message);
+            switch (msg.Type)
+            {
+                case MessageType.Hello:
+                    HandleHello(msg);
+                    break;
+                case MessageType.RequestData:
+                    SendData();
+                    break;
+                case MessageType.Ack:
+                    Console.WriteLine(msg.Content);
+                    HandleAck(msg.Content);
+                    break;
+                    // case MessageType.Error:
+                    //     HandleError();
+                    //     break;
+            }
+        }
+    }
+
+    // you can call a dedicated method to handle each received type of messages
+    //TODO: [Receive Hello]
+    //TODO: [Send Welcome]
+    public void HandleHello(Message msg)
+    {
+        Console.WriteLine("Hello message received\n");
+
+        if (msg.Content != null)
+        {
+            threshold = int.Parse(msg.Content);
+            SendWelcome();
+        }
+        // HandleError();
+
+
+
 
     }
 
-    //TODO: create all needed objects for your sockets 
+    public void SendWelcome()
+    {
+        Message welcome_message = new Message();
+        welcome_message.Type = MessageType.Welcome;
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(welcome_message));
+        sock.SendTo(msgBytes, remoteEP);
+        Console.WriteLine("Send welcome message\n");
+    }
 
-    //TODO: keep receiving messages from clients
-    // you can call a dedicated method to handle each received type of messages
 
-    //TODO: [Receive Hello]
 
-    //TODO: [Send Welcome]
 
-    //TODO: [Receive RequestData]
+    public void SendData(int ChunckToSend = 0)
+    {
+        string filePath = "hamlet.txt";
+        string[] lines = File.ReadAllLines(filePath);
+        //get all the lines into a array
+
+
+        int chunkSize = 100; // Define the size of each message chunk
+
+        int totalChunks = (int)Math.Ceiling((double)lines.Length / chunkSize); // get the amount of chunks depending on chucksize
+
+        if (ChunckToSend <= totalChunks)
+        {
+            string MessageID = 000 + ChunckToSend.ToString("D3");//append the messageID in numbers of 4: 0001, 0002, 0003 etc.
+            int start = ChunckToSend * chunkSize;//get the start of the chunk
+            int end = Math.Min(start + chunkSize, lines.Length);//get the end
+            List<string> MessageContent = lines.Skip(start).Take(end - start).ToList();//make the array into 1 string
+            Console.WriteLine("Message Content:");
+
+            Message data = new Message
+            {
+                Type = MessageType.Data,
+                Content = MessageID + "" + string.Join(Environment.NewLine, MessageContent)
+            };
+            Console.WriteLine(data.Content);
+
+            SendAck(data, MessageID);
+        }
+        else
+        {
+            SendEnd();
+        }
+
+
+
+    }
+
+    //TODO: [End sending data to client]
+    public void SendEnd()
+    {
+        Console.WriteLine("Send end message\n");
+        // Perform any necessary cleanup or finalization here
+        // For example, close the socket or release any resources
+        Message msg = new Message();
+        msg.Type = MessageType.End;
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
+        sock.SendTo(msgBytes, remoteEP);
+    }
+
+    public void HandleAck(string msgID)
+    {
+        Console.WriteLine("Ack message received with message ID: " + msgID);
+        int NextChunk = int.Parse(msgID);
+        NextChunk++;
+        SendData(NextChunk);
+
+    }
+
+    public void SendAck(Message msg, string MessageID)
+    {
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
+        try
+        {
+            sock.SendTo(msgBytes, remoteEP);
+            Console.WriteLine("Data message sent ID: " + MessageID);
+            Console.WriteLine("Waiting for Acknowledgement...");
+            ReceiveMessage();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error sending data message: " + ex.Message);
+            //handleerror
+        }
+
+
+    }
 
     //TODO: [Send Data]
 
-    //TODO: [Implement your slow-start algorithm considering the threshold] 
+    //TODO: [Implement your slow-start algorithm considering the threshold]
+
+
 
     //TODO: [End sending data to client]
+
+    //TODO: [Send Error]
+    public void SendError()
+    {
+        Console.WriteLine("Send error message");
+        Message msg = new Message();
+        msg.Type = MessageType.Error;
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
+        sock.SendTo(msgBytes, remoteEP);
+
+    }
+
 
     //TODO: [Handle Errors]
 
