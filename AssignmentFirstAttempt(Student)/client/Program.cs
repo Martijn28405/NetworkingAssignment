b504e04300c0df1cbb7a06e6c72c9df937ef9bea
DoubleKimp <karstenkeemink@gmail.com>
2024-04-29 21:46:22 +0200

    IT WORROK

--- AssignmentFirstAttempt(Student)/client/Program.cs
+++ AssignmentFirstAttempt(Student)/client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
@@ -17,136 +18,174 @@ class Program
 
 class ClientUDP
 {
-    //TODO: create all needed objects for your sockets
-    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-    byte[] buffer = new byte[66000];
-    static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-    static IPEndPoint ServerEndpoint = new IPEndPoint(ipAddress, 32000);
-    static EndPoint remoteEP = new IPEndPoint(ipAddress, 32000);
 
-    public int threshold = 20;
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
-
     {
         SendHelloMessage();
         ReceiveMessage();
     }
+    //TODO: create all needed objects for your sockets ✓
+    private Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    private static byte[] buffer = new byte[66000];
+    private static IPAddress ipAddress = NetworkInterface.GetAllNetworkInterfaces()
+        .SelectMany(nic => nic.GetIPProperties().UnicastAddresses)
+        .Where(ua => ua.Address.AddressFamily == AddressFamily.InterNetwork && !IPAddress.IsLoopback(ua.Address))
+        .Select(ua => ua.Address)
+        .FirstOrDefault() ?? IPAddress.None;
+    private static IPEndPoint ServerEndpoint = new IPEndPoint(ipAddress, 32000);
+    private static EndPoint remoteEP = new IPEndPoint(ipAddress, 32000);
 
-    //TODO: [Send Hello message]
-    public void SendHelloMessage()
+    private int threshold = 20;
+
+
+    // recieved messages
+    private SortedDictionary<string, string> recievedData = new SortedDictionary<string, string>();
+
+    //TODO: [Send Hello message] ✓
+    private void SendHelloMessage()
     {
-        s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        Message msg = new Message();
-        msg.Type = MessageType.Hello;
-        msg.Content = threshold.ToString(); // Default threshold value
-        string json = JsonSerializer.Serialize(msg);
-        byte[] data = Encoding.ASCII.GetBytes(json);
-        s.SendTo(data, ServerEndpoint);
-        Console.WriteLine("Hello message sent to the server");
+        Message Hello = new Message
+        {
+            Type = MessageType.Hello,
+            Content = $"{threshold}"
+        };
+        byte[] bytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(Hello));
+        s.SendTo(bytes, ServerEndpoint);
     }
 
-    public void ReceiveMessage()
+    private void ReceiveMessage()
     {
         while (true)
         {
-            int recv = s.ReceiveFrom(buffer, ref remoteEP);
-            string message = Encoding.ASCII.GetString(buffer, 0, recv);
-            Message msg = JsonSerializer.Deserialize<Message>(message);
-            switch (msg.Type)
+            Console.WriteLine("Awaiting message...");
+            
+            try
             {
-                case MessageType.Welcome:
-                    Console.WriteLine("Welcome message received");
-                    SendRequestData();
-                    break;
-                case MessageType.Data:
-                    HandleData(msg);
+                s.ReceiveTimeout = 5000;
+                int recv = s.ReceiveFrom(buffer, ref remoteEP);
+                string message = Encoding.ASCII.GetString(buffer, 0, recv);
+                Message msg = JsonSerializer.Deserialize<Message>(message)!;
+                switch (msg.Type)
+                {
+                    case MessageType.Welcome:
+                        Console.WriteLine("Welcome message received\nSending RequestData message\n");
+                        SendRequestData();
+                        break;
 
-                    break;
-                case MessageType.End:
-                    HandleEnd();
-                    break;
-                    // case MessageType.Error:
-                    //     HandleError();
-                    //     break;
+                    case MessageType.Data:
+                        if (msg.Content != null)
+                        {
+                            Console.WriteLine(msg.Content.Split(" ").Length);
+                            RecieveData(msg.Content);
+                        }
+                        break;
+
+                    case MessageType.End:
+                        HandleEnd();
+                        break;
+                }
+                
             }
-
+            catch (SocketException e)
+            {
+                if (e.SocketErrorCode == SocketError.TimedOut)
+                {
+                    HandleError();
+                    break;
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 
-    //TODO: [Receive Welcome]
-
     //TODO: [Send RequestData]
-    public void SendRequestData()
+    private void SendRequestData()
     {
-        Message msg = new Message();
-        msg.Type = MessageType.RequestData;
-        msg.Content = "hamlet.txt";
-        string json = JsonSerializer.Serialize(msg);
-        byte[] data = Encoding.ASCII.GetBytes(json);
-        s.SendTo(data, ServerEndpoint);
-        Console.WriteLine("RequestData message sent to the server");
+        Message DataRequest = new Message
+        {
+            Type = MessageType.RequestData,
+            Content = "hamlet.txt"
+        };
+        byte[] data = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(DataRequest));
+        try
+        {
+            s.SendTo(data, ServerEndpoint);
+            Console.WriteLine("RequestData message sent to the server");
+        }
+        catch (SocketException)
+        {
+            HandleError();
+        }
     }
 
     //TODO: [Receive Data]
-    public void HandleData(Message msg)
-    {
-        Console.WriteLine("Data message received");
+    private void RecieveData(string messageContent){        
         try
         {
-            string msgID = msg.Content.Substring(0, 4);
-            string msgData = msg.Content.Substring(4);
-            if (msgID == "0000")
+            string messageID = messageContent.Substring(0, 4);
+            string messageData = messageContent.Substring(4);
+            Console.WriteLine($"Data message received with Id: {messageID}");
+            if (messageID == "0001")
             {
                 File.Delete("output.txt");
             }
-            File.AppendAllText("output.txt", msgData);
-            SendAck(msgID);
+            recievedData[messageID] = messageData;
+            SendAck(messageID);
         }
         catch
         {
-            Console.WriteLine("Error: message is empty");
+            Console.WriteLine("Error: message is empty or faulty");
             //handleerror
         }
 
-
     }
 
-    public void SendAck(string id)
-    {
-        Message msg = new Message
-        {
+    private void SendAck(string messageid){
+        Message ackMessage = new Message{
             Type = MessageType.Ack,
-            Content = id
+            Content = messageid
         };
-        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
-        s.SendTo(msgBytes, ServerEndpoint);
-        Console.WriteLine("Ack message sent to the server");
+        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(ackMessage));
+        try{
+            s.SendTo(msgBytes, ServerEndpoint);
+            Console.WriteLine("Ack message sent to the server");
+        }
+        catch(SocketException){
+            HandleError();
+        }
     }
 
-    //TODO: [Send End]
-    public void HandleEnd()
+    //TODO: [Recieve End]
+    private void HandleEnd()
     {
         Console.WriteLine("End message received");
         // Perform any necessary cleanup or finalization here
         // For example, close the socket or release any resources
+        if (s != null && s.Connected)
+        {   
+            Console.WriteLine("Closing socket");
+            s.Close();
+        }
+        //Making the output file
+        Console.WriteLine("Making output.txt");
+        foreach (var data in recievedData)
+        {
+            File.AppendAllText("output.txt", data.Value);
+        }
+        // Terminate the application
+        Console.WriteLine("Terminating client...");
         Environment.Exit(0);
-
-    }
-
-    //TODO: [Send Error]
-    public void HandleError()
-    {
-        Console.WriteLine("Error message received");
-
-        // Perform any necessary error handling here
-
-
     }
 
 
     //TODO: [Handle Errors]
-
-
+    private void HandleError()
+    {
+        Environment.Exit(1);
+    }
 }
--- AssignmentFirstAttempt(Student)/server/Program.cs
+++ AssignmentFirstAttempt(Student)/server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlTypes;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -21,188 +22,338 @@ class Program
 
 class ServerUDP
 {
-    //TODO: create all needed objects for your sockets 
+    //TODO: implement all necessary logic to create sockets and handle incoming messages
+    //TODO: create all needed objects for your sockets ✓
+
     private byte[] buffer = new byte[66000];
-    private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    private static IPAddress ipAddress = NetworkInterface.GetAllNetworkInterfaces()
+        .SelectMany(nic => nic.GetIPProperties().UnicastAddresses)
+        .Where(ua => ua.Address.AddressFamily == AddressFamily.InterNetwork && !IPAddress.IsLoopback(ua.Address))
+        .Select(ua => ua.Address)
+        .FirstOrDefault() ?? IPAddress.None;
     private static IPEndPoint serverIpEndPoint = new IPEndPoint(ipAddress, 32000);
     private EndPoint remoteEP = new IPEndPoint(ipAddress, 32000);
+    private Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-    Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    // Is a client connected?
+    private bool got_Connection = false;
+
+    // further connection requests get put into a Queue
+    private Queue<Message> connectionRequests = new Queue<Message>();
+
+    //Data Info
+    private int Threshold = 1;
+
+    private string filePath = "";
+    private string[] fileLines = new string[0];
+
+    //Make Queue with chunks to send
+    private Dictionary<string, string[]>? dataChunks;
+
+    //acks recieving
+    private List<string> sendMessages = new List<string>();
 
 
-    static int threshold = 0;
-
-
-    //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
     {
         sock.Bind(serverIpEndPoint);
-        ReceiveMessage();
+        RetrieveConnection();
     }
 
-    //TODO: keep receiving messages from clients
-    public void ReceiveMessage()
-    {
-        Console.WriteLine("server is listening on port 32000");
 
-        while (true)
+    //TODO: keep receiving messages from clients
+    // you can call a dedicated method to handle each received type of messages
+    public void RetrieveConnection()
+    {
+        Console.WriteLine($"server is waiting for connection on port: {serverIpEndPoint} ...");
+
+        while (!got_Connection)
         {
-            Console.WriteLine("server is waiting for a message.....");
-            int recv = sock.ReceiveFrom(buffer, ref remoteEP);
-            string message = Encoding.ASCII.GetString(buffer, 0, recv);
-            Console.WriteLine("Received message: " + message + "\n");
-            Message msg = JsonSerializer.Deserialize<Message>(message);
-            switch (msg.Type)
+            try
             {
+                if (connectionRequests.Count == 0){
+                    // Receive message
+                    int received = sock.ReceiveFrom(buffer, ref remoteEP);
+
+                    // Handle message
+                    string message = Encoding.ASCII.GetString(buffer, 0, received);
+                    Console.WriteLine("Received message: " + message + "\n");
+                    Message decryptedMessage = JsonSerializer.Deserialize<Message>(message)!;
+                    // Handle message based on message type
+                    HandleConnection(decryptedMessage);
+                }else{
+                    HandleConnection(connectionRequests.Dequeue());
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log error
+                Console.WriteLine("Error: " + ex.Message);
+                // Go back to listening for a hello message
+                got_Connection = false;
+                continue;
+            }
+        }
+        // Set a timeout for 5 seconds
+        sock.ReceiveTimeout = 5000;
+        
+        try
+        {
+            while (got_Connection)
+            {
+                try{
+                    // Receive message after establishing a connection
+                    int requestRecieved = sock.ReceiveFrom(buffer, ref remoteEP);
+                    // Handle message
+                    string requestMessage = Encoding.ASCII.GetString(buffer, 0, requestRecieved);
+                    Message decryptedRequestMessage = JsonSerializer.Deserialize<Message>(requestMessage)!;
+            
+                    switch (decryptedRequestMessage.Type)
+                    {
+                        case MessageType.RequestData:
+                            if (decryptedRequestMessage.Content != null)
+                            {
+                                ReceiveRequestData(decryptedRequestMessage.Content);
+                            }
+                            else
+                            {
+                                HandleError(new Exception("Null message content received."));
+                            }
+                            return; // Exit the loop when a RequestData message is received
+                        case MessageType.Hello:
+                            Console.WriteLine("Received Hello message");
+                            connectionRequests.Enqueue(decryptedRequestMessage); // Store the message in the queue to connect after ending this connection
+                            break;
+                        default:
+                            HandleError(new Exception($"Unwanted message received: {decryptedRequestMessage}"));
+                            break;
+                    }
+                }catch (Exception ex)
+                {
+                    // Log error
+                    HandleError(ex);
+                    // Go back to listening for a hello message
+
+                    got_Connection = false;
+                    continue;
+                }
+
+            }
+        }
+        catch (SocketException e) when (e.SocketErrorCode == SocketError.TimedOut)
+        {
+            // If no request data message is received within 5 seconds, call the SendEnd method
+            SendEnd();
+        }
+    }
+    private void HandleConnection(Message decryptedmessage)
+    {
+        try
+        {
+            switch (decryptedmessage.Type)
+            {
+                //TODO: [Receive Hello] ✓
                 case MessageType.Hello:
-                    HandleHello(msg);
+                    if (int.TryParse(decryptedmessage.Content, out int threshold))
+                    {
+                        Threshold = threshold;
+                        Console.WriteLine("Hello message received. Threshold: " + Threshold);
+                        SendWelcome();
+                    }
+                    else
+                    {
+                        HandleError(new Exception("Invalid threshold value"));
+                    }
                     break;
-                case MessageType.RequestData:
-                    SendData();
+
+                default:
+                    HandleError(new Exception("Invalid message type"));
                     break;
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleError(ex);
+        }
+    }
+
+    //TODO: [Send Welcome] ✓
+    private void SendWelcome()
+    {
+        Message welcomeMessage = new Message { Type = MessageType.Welcome };
+        byte[] messageBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(welcomeMessage));
+        try
+        {
+            sock.SendTo(messageBytes, remoteEP);
+            Console.WriteLine("Send welcome message\n");
+            got_Connection = true;
+        }
+        catch (SocketException ex)
+        {
+            HandleError(ex);
+        }
+    }
+
+    //TODO: [Receive RequestData]
+    private void ReceiveRequestData(string messageContent)
+    {
+        Console.WriteLine($"Data requested: {messageContent}");
+        filePath = messageContent;
+        fileLines = File.ReadAllText(filePath).Split('\n');
+        dataChunks = DevideData(fileLines);
+        SendData();
+    }
+
+    private Dictionary<string, string[]> DevideData(string[] filelines)
+    {
+        var dataDict = new Dictionary<string, string[]>();
+        int chunksize = 100;
+        string messageID = "0001";
+
+        for (int i = 0; i <= fileLines.Length; i += chunksize)
+        {
+            int end = Math.Min(i + chunksize, filelines.Length);
+            string[] chunk = filelines.Skip(i).Take(end - i).ToArray();
+            dataDict.Add(messageID, chunk);
+            int nextMessageID = int.Parse(messageID) + 1;
+            messageID = nextMessageID.ToString("D4");
+        }
+
+        return dataDict;
+    }
+    //TODO: [Send Data]
+    private void SendData()
+    {
+        // Initialize congestion window size to 1 (or any small value)
+        int windowSize = 1;
+        sendMessages = new List<string>();
+
+        // While there is data to send
+        while (dataChunks != null && dataChunks.Count > 0)
+        {
+            // Create a queue to hold the chunks of data to be sent
+            Queue<KeyValuePair<string, string[]>> chunksQueue = new Queue<KeyValuePair<string, string[]>>(dataChunks);
+            
+            for (int i = 0; i < windowSize && chunksQueue.Count > 0; i++)
+            {
+                // Get the next chunk of data to send
+                var chunk = chunksQueue.Dequeue();
+            
+                // Create a message with the chunk data
+                Message dataMessage = new Message
+                {
+                    Type = MessageType.Data,
+                    Content = chunk.Key + "" + string.Join("", chunk.Value)
+                };
+            
+                // Convert the message to bytes
+                byte[] messageBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(dataMessage));
+            
+                // Send the message
+                try
+                {
+                    sock.SendTo(messageBytes, remoteEP);
+                    sendMessages.Add(chunk.Key);
+                }
+                catch (SocketException ex) 
+                {
+                    HandleError(ex);
+                }
+            }
+            Console.WriteLine($"Send messages with Id's:\n[{string.Join(", ", sendMessages)}]\n");
+
+            // Wait for acknowledgements from the client
+            sock.ReceiveTimeout = 1000;
+            try{WaitForAcks();}
+            catch(SocketException ex) when (ex.SocketErrorCode == SocketError.TimedOut){
+                if (windowSize > 1){
+                    windowSize /= 2;
+                }
+                HandleError(ex);
+            }
+
+            if (sendMessages.Count == 0)
+            {
+                // If all acknowledgements are received, increase the window size
+                if (windowSize < Threshold)
+                {
+                    // Double the congestion window size
+                    windowSize *= 2;
+                    if(windowSize > Threshold){
+                        windowSize = Threshold;
+                    }
+                }
+            }
+            else
+            {
+                // If a timeout or lost acknowledgement occurs, set the threshold to windowSize / 2, reset windowSize to the previous attempt, and resend the unacked messages
+                Threshold = windowSize / 2;
+                windowSize /= 2;
+                sendMessages.Clear();
+            }
+        }
+        Console.WriteLine("Send all data!");
+        Console.WriteLine("Recieved all acknowledgements");
+        SendEnd();
+    }
+
+    private void WaitForAcks()
+    {
+        while(sendMessages.Count > 0){
+            // Receive ack message
+            int received = sock.ReceiveFrom(buffer, ref remoteEP);
+
+            // Handle ack message
+            string message = Encoding.ASCII.GetString(buffer, 0, received);
+            Message DecryptedMessage = JsonSerializer.Deserialize<Message>(message)!;
+            Console.WriteLine($"Recieved {DecryptedMessage.Type} for messageID: {DecryptedMessage.Content}\n");
+            switch (DecryptedMessage.Type)
+            {
                 case MessageType.Ack:
-                    Console.WriteLine(msg.Content);
-                    HandleAck(msg.Content);
+                    if (DecryptedMessage.Content != null)
+                    {
+                        sendMessages.Remove(DecryptedMessage.Content);
+                        dataChunks?.Remove(DecryptedMessage.Content);
+                    }
                     break;
-                    // case MessageType.Error:
-                    //     HandleError();
-                    //     break;
+                case MessageType.Hello:
+                    connectionRequests.Enqueue(DecryptedMessage);
+                    break;
+                default:
+                    HandleError(new Exception($"Unexpected message recieved: {DecryptedMessage}"));
+                    break;
             }
         }
     }
 
-    // you can call a dedicated method to handle each received type of messages
-    //TODO: [Receive Hello]
-    //TODO: [Send Welcome]
-    public void HandleHello(Message msg)
-    {
-        Console.WriteLine("Hello message received\n");
-
-        if (msg.Content != null)
-        {
-            threshold = int.Parse(msg.Content);
-            SendWelcome();
-        }
-        // HandleError();
-
-
-
-
-    }
-
-    public void SendWelcome()
-    {
-        Message welcome_message = new Message();
-        welcome_message.Type = MessageType.Welcome;
-        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(welcome_message));
-        sock.SendTo(msgBytes, remoteEP);
-        Console.WriteLine("Send welcome message\n");
-    }
-
-
-
-
-    public void SendData(int ChunckToSend = 0)
-    {
-        string filePath = "hamlet.txt";
-        string[] lines = File.ReadAllLines(filePath);
-        //get all the lines into a array
-
-
-        int chunkSize = 100; // Define the size of each message chunk
-
-        int totalChunks = (int)Math.Ceiling((double)lines.Length / chunkSize); // get the amount of chunks depending on chucksize
-
-        if (ChunckToSend <= totalChunks)
-        {
-            string MessageID = 000 + ChunckToSend.ToString("D3");//append the messageID in numbers of 4: 0001, 0002, 0003 etc.
-            int start = ChunckToSend * chunkSize;//get the start of the chunk
-            int end = Math.Min(start + chunkSize, lines.Length);//get the end
-            List<string> MessageContent = lines.Skip(start).Take(end - start).ToList();//make the array into 1 string
-            Console.WriteLine("Message Content:");
-
-            Message data = new Message
-            {
-                Type = MessageType.Data,
-                Content = MessageID + "" + string.Join("\n", MessageContent)
-            };
-            Console.WriteLine(data.Content);
-
-            SendAck(data, MessageID);
-        }
-        else
-        {
-            SendEnd();
-        }
-
-
-
-    }
-
-    //TODO: [End sending data to client]
-    public void SendEnd()
-    {
-        Console.WriteLine("Send end message\n");
-        // Perform any necessary cleanup or finalization here
-        // For example, close the socket or release any resources
-        Message msg = new Message();
-        msg.Type = MessageType.End;
-        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
-        sock.SendTo(msgBytes, remoteEP);
-    }
-
-    public void HandleAck(string msgID)
-    {
-        Console.WriteLine("Ack message received with message ID: " + msgID);
-        int NextChunk = int.Parse(msgID);
-        NextChunk++;
-        SendData(NextChunk);
-
-    }
-
-    public void SendAck(Message msg, string MessageID)
-    {
-        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
-        try
-        {
-            sock.SendTo(msgBytes, remoteEP);
-            Console.WriteLine("Data message sent ID: " + MessageID);
-            Console.WriteLine("Waiting for Acknowledgement...");
-            ReceiveMessage();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error sending data message: " + ex.Message);
-            //handleerror
-        }
-
-
-    }
-
-    //TODO: [Send Data]
-
-    //TODO: [Implement your slow-start algorithm considering the threshold]
-
-
-
-    //TODO: [End sending data to client]
-
-    //TODO: [Send Error]
-    public void SendError()
-    {
-        Console.WriteLine("Send error message");
-        Message msg = new Message();
-        msg.Type = MessageType.Error;
-        byte[] msgBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(msg));
-        sock.SendTo(msgBytes, remoteEP);
-
-    }
-
-
     //TODO: [Handle Errors]
+    private void HandleError(Exception error)
+    {
+        Console.WriteLine("Handle Error called");
+        Console.WriteLine("Error: " + error.Message);
+        // Reset the server or perform any necessary cleanup here
+        // if (sock != null && sock.Connected)
+        // {
+        //     sock.Close();
+        // }
+        // start();        
+    }
 
-    //TODO: create all needed methods to handle incoming messages
-
-
+    //Send the end message and reset all the timeouts, also restart the connection on RetrieveConnection
+    private void SendEnd(){
+        Console.WriteLine("Sending end message...\n");
+        Message endMessage = new Message{
+            Type = MessageType.End
+        };
+        byte[] messageBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(endMessage));
+        try{
+            sock.SendTo(messageBytes, remoteEP);
+            got_Connection = false;
+        }catch(SocketException ex){
+            HandleError(ex);
+        }
+        sock.ReceiveTimeout = 0;
+        RetrieveConnection();
+    }
 }
